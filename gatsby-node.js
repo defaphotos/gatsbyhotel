37982574a12482e8566b9d57367f56ece3dfb799
defaphotos <defaphotos@gmail.com>
2020-05-24 22:24:04 -0400

    Updated

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,27 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+exports.createPages =async({actions,graphql,reporter})=>{
+    const resultados = await graphql(`
+    query{
+        allDatoCmsHabitacion{
+          nodes{
+            slug
+          }
+        }
+      }
+    `);
 
-// You can delete this file if you're not using it
+    if(resultados.errors){
+        reporter.panic('Hubo un error',resultados.errors);
+    }
+
+    const habitaciones = resultados.data.allDatoCmsHabitacion.nodes;
+
+    habitaciones.forEach(habitacion =>{
+      actions.createPage({
+        path: habitacion.slug,
+        component: require.resolve('./src/components/habitaciones.js'),
+        context: {
+          slug: habitacion.slug
+        }
+      })
+    })
+}
